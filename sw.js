3a95357636512bcd2fbece44ccc9b47c953330ca
Nitai Sarkar <140227933+NitaiStudio@users.noreply.github.com>
2025-12-15 21:49:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'anoncall-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
+    'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
+    'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
